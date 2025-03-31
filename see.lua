3f95fc906ca48616b6dc3c1a91bc973fe09d14d7
Ecohub-1 <boyyanamalila@gmail.com>
2025-04-01 02:03:10 +0700

    Create see.lua

---
+++ see.lua
@@ -0,0 +1,61 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "ECO HUB " .. Fluent.Version,
+    SubTitle = " | by zero9ZX",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "align-left" }),
+    Teleport = Window:AddTab({ Title = "Teleport", Icon = "layers" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
+}
+local Options = Fluent.Options
+
+do
+    Fluent:Notify({
+        Title = "Eco Hub",
+        Content = "",
+        SubContent = "Thank for play", 
+        Duration = 10-- Set to nil to make the notification not disappear
+    })
+
+
+
+    Tabs.Teleport:AddParagraph({
+        Title = "Teleport",
+        Content = "tp to island"
+    })
+
+
+
+    Tabs.Teleport:AddButton({
+        Title = "Solo leveling",
+        Description = "tp to Solo leveling city",
+        Callback = function()
+            Window:Dialog({
+                Title = "TP",
+                Content = "Are you sure you want to teleport?",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+              Delays = 10; target = CFrame.new(576.453369140625, 28.434574127197266, 272.19970703125)
+Tween = TweenInfo.new(Delays, Enum.EasingStyle.Linear)
+local tween = game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,Tween,{CFrame = target})
+tween:Play()
+tween.Completed:Wait()
+                        end
+                    },
+                }
+            })
+        end
+    })
+end
