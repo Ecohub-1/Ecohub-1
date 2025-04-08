ebb6bb20821515300d361f042067ae09b12598ff
Ecohub-1 <boyyanamalila@gmail.com>
2025-04-09 04:30:38 +0700

    Update see.lua

--- see.lua
+++ see.lua
@@ -1,1 +1,22 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
+local Window = Fluent:CreateWindow({
+    Title = "Eco Hub" .. Fluent.Version,
+    SubTitle = " | by zer09Xz",
+    TabWidth = 150,
+    Size = UDim2.fromOffset(580, 400),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+local Tabs = {
+    credit = Window:AddTab({ Title = "credit", Icon = "" })
+    Auto Farm = Window:AddTab({ Title = "Auto Farm", Icon = "" }),
+    Auto Boss = Window:AddTab({ Title = "Main", Icon = "" }),
+    Dungeon = Window:AddTab({ Title = "Dungeon", Icon = "" }),
+    Teleport = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
