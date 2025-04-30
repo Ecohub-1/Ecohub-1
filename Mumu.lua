792922db12108deb258c97af15967fbf2acdb030
Ecohub-1 <boyyanamalila@gmail.com>
2025-04-30 13:18:53 +0700

    Update Mumu.lua

--- Mumu.lua
+++ Mumu.lua
@@ -109,7 +109,7 @@ end
 
 local DD = Tabs.Main:AddDropdown({
     Title = "Select Monster",
-    Value = monNames,
+    Options = monNames,
     Default = "",
     Callback = function(v)
         selMon = v
