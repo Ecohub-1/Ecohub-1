098e14a09b0b2ab69c2b34f1bea9cb6e778e20d4
Ecohub-1 <boyyanamalila@gmail.com>
2025-05-06 03:37:18 +0700

    Create Mumu.lua

---
+++ Mumu.lua
@@ -0,0 +1,38 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Eco Hub 1.0",
+    SubTitle = " | by zer09Xz",
+    TabWidth = 150,
+    Size = UDim2.fromOffset(580, 400),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Auto Farm", Icon = "bookmark" }),
+    Game = Window:AddTab({ Title = "Game", Icon = "gamepad-2" }),
+    Other = Window:AddTab({ Title = "Other", Icon = "candy" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
+}
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local autoPlayValue = ReplicatedStorage:WaitForChild("Player_Data"):WaitForChild(player.Name):WaitForChild("Data"):WaitForChild("AutoPlay")
+
+local autoPlayLoop = false
+local AutoPlay = Tabs.Game:AddToggle("AutoPlay", { Title = "Auto Play", Default = false })
+
+AutoPlay:OnChanged(function(value)
+    autoPlayLoop = value
+end)
+
+task.spawn(function()
+    while task.wait(2) do
+        if autoPlayLoop and not autoPlayValue.Value then
+            ReplicatedStorage.Remote.Server.Units.AutoPlay:FireServer()
+       end
+    end
+end)
