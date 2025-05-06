e9fe991c0e619d7fd8fbbe0d58be42c36441c786
Ecohub-1 <boyyanamalila@gmail.com>
2025-05-07 02:53:03 +0700

    Update Bloxspin.lua

--- Bloxspin.lua
+++ Bloxspin.lua
@@ -24,48 +24,27 @@ local VotePlay = ReplicatedStorage.Remote.Server.OnGame.Voting.VotePlaying:FireS
 
 
 
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+getgenv().AVP = false
 
-local Window = Fluent:CreateWindow({
-    Title = "Eco Hub 1.0",
-    SubTitle = " | by zer09Xz",
-    TabWidth = 150,
-    Size = UDim2.fromOffset(580, 400),
-    Acrylic = true,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
+Tabs.Game:AddToggle("AVP", {
+    Title = "Auto VotePlay",
+    Default = false, 
+    Callback = function(V)
+        getgenv().AVP = V
+    end
 })
 
-local Tabs = {
-    Main = Window:AddTab({ Title = "Auto Farm", Icon = "bookmark" }),
-    Game = Window:AddTab({ Title = "Game", Icon = "gamepad-2" }),
-    Other = Window:AddTab({ Title = "Other", Icon = "candy" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" }),
-}
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local VotePlay = ReplicatedStorage.Remote.Server.OnGame.Voting.VotePlaying:FireServer()
-
-
-
-getgenv().AVP = false  
-Tabs.Game:AddToggle("AVP", {
-   Title = "Auto VotePlay",
-   Default = false, 
-   Callback = function(V)
-        getgenv().AVP = V
-        if V then
-            spawn(function()
-                while getgenv().AVP and task.wait(1) do
-                    local inGame = player:WaitForChild("PlayerGui"):WaitForChild("HUD"):WaitForChild("InGame")
-                    local votePlaying = inGame:WaitForChild("VotePlaying")
-                    if votePlaying:IsA("GuiObject") and votePlaying.Visible then
-                        VotePlay:FireServer()
-                    end
+spawn(function()
+    while true do
+        if getgenv().AVP then
+            pcall(function()
+                local inGame = player:WaitForChild("PlayerGui"):WaitForChild("HUD"):WaitForChild("InGame")
+                local votePlaying = inGame:FindFirstChild("VotePlaying")
+                if votePlaying and votePlaying:IsA("GuiObject") and votePlaying.Visible then
+                    VotePlay:FireServer()
                 end
             end)
         end
-   end
-})
+        task.wait(1)
+    end
+end)
